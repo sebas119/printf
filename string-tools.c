407bcdd6b063858fb162b16b1f2e1b1102e0fa50
Noah172 <55119647+Noah172@users.noreply.github.com>
2019-10-29 15:09:10 -0500

    update

--- string-tools.c
+++ string-tools.c
@@ -98,3 +98,21 @@ char *itoa(int value, char *buffer, int base)
 	rev_string(buffer);
 	return (buffer);
 }
+
+/**
+ * *string_toupper - Changes all lowercase letters of a string to uppercase
+ * @a: Pointer to a char array char[]
+ * Return: string uppercase
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] -= 32;
+		i++;
+	}
+	return (a);
+}
